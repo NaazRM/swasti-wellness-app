aea68f97e612a9a6472b6ce6572ef1d82865038b
NaazRM <120334558+NaazRM@users.noreply.github.com>
2025-07-01 02:11:07 +0530

    Fix Built with Bolt.new Attribution Visibility

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,12 @@ import BottomNavigation from './BottomNavigation';
 const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      <main className="flex-1 pb-24">
+      <main className="flex-1 pb-32">
         <Outlet />
       </main>
       
-      {/* Built with Bolt.new attribution - positioned above bottom nav */}
-      <div className="bg-white border-t border-gray-100 py-2 px-4">
+      {/* Built with Bolt.new attribution - positioned above bottom nav with higher z-index */}
+      <div className="fixed bottom-16 left-0 right-0 bg-white border-t border-gray-100 py-2 px-4 z-40">
         <p className="text-center text-xs text-gray-500">
           Built with{' '}
           <a 
